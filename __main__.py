874c71bbbc05abd77d8da8118497c0d422c32333
Tommy Smith <tommy@tommyesmith.com>
2022-06-14 15:50:06 -0400

    add docker container upload

--- __main__.py
+++ __main__.py
@@ -1,5 +1,8 @@
+import json
+
 import pulumi
 import pulumi_aws as aws
+
 
 cluster = aws.ecs.Cluster('cluster')
 
@@ -43,3 +46,63 @@ wl = aws.lb.Listener(
     port=80,
     default_actions=[{'type': 'forward', 'target_group_arn': atg.arn}]
 )
+
+role = aws.iam.Role(
+    'task-exec-role',
+    assume_role_policy=json.dumps({
+        'Version': '2008-10-17',
+        'Statement': [{
+            'Sid': '',
+            'Effect': 'Allow',
+            'Principal': {
+                'Service': 'ecs-tasks.amazonaws.com'
+            },
+            'Action': 'sts:AssumeRole'
+        }]
+    }))
+
+rpa = aws.iam.RolePolicyAttachment(
+    'task-exec-policy',
+    role=role.name,
+    policy_arn='arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy'
+)
+
+task_definition = aws.ecs.TaskDefinition(
+    'app-task',
+    family='fargate-task-definition',
+    cpu='256',
+    memory='512',
+    network_mode='awsvpc',
+    requires_compatibilities=['FARGATE'],
+    execution_role_arn=role.arn,
+    container_definitions=json.dumps([{
+        'name': 'my-app',
+        'image': 'nginx',
+        'portMappings': [{
+            'containerPort': 80,
+            'hostPort': 80,
+            'protocol': 'tcp'
+        }]
+    }])
+)
+
+service = aws.ecs.Service(
+    'app-svc',
+    cluster=cluster.arn,
+    desired_count=1,
+    launch_type='FARGATE',
+    task_definition=task_definition.arn,
+    network_configuration={
+        'assign_public_ip': 'true',
+        'subnets': vpc_subnets.ids,
+        'security_groups': [group.id]
+    },
+    load_balancers=[{
+        'target_group_arn': atg.arn,
+        'container_name': 'my-app',
+        'container_port': 80
+    }],
+    opts=pulumi.ResourceOptions(depends_on=[wl])
+)
+
+pulumi.export('url', alb.dns_name)
